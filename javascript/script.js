2f2783a0310d3421090a0676e1f3e1ab49aabedf
chigozie emmanuel <mmajoy55@gmail.com>
2023-07-13 11:13:58 +0100

    'made alot of changes'

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+// for image slider/
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+      items: 1,
+      loop: true,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      smartSpeed: 900,
+      autoplayHoverPause: true,
+  
+    });
+  });
+  // the end/
+
+
